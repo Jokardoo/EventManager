794d8e1fa641e7fb628ec9cc00d8fc52cf4b5400
Jokardo <Jokardoo@mail.ru>
2024-10-23 22:26:51 +0300

    Провел ручные тесты аутентификации в обход AuthenticationManager

--- src/main/java/jokardoo/eventmanager/security/jwt/JwtAuthenticationService.java
+++ src/main/java/jokardoo/eventmanager/security/jwt/JwtAuthenticationService.java
@@ -1,14 +1,20 @@
 package jokardoo.eventmanager.security.jwt;
 
 import jokardoo.eventmanager.domain.user.SignUpRequest;
+import jokardoo.eventmanager.domain.user.UserEntity;
+import jokardoo.eventmanager.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class JwtAuthenticationService {
+    //TEST FIELDS
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
     private final JwtTokenManager tokenManager;
     private final AuthenticationManager authenticationManager;
@@ -21,8 +27,28 @@ public class JwtAuthenticationService {
                 )
         );
 
+        // TEST AUTHENTICATION
 
-        return tokenManager.generateToken(signUpRequest.getLogin());
+        UserEntity userEntity = userRepository
+                .findByLogin(signUpRequest.getLogin())
+                .orElseThrow(() -> new IllegalArgumentException("User with login " +
+                        signUpRequest.getLogin() + " not found!"));
+
+        if (userEntity.getLogin().equals(signUpRequest.getLogin())) {
+            System.out.println("Right login");
+
+            if (passwordEncoder.matches(signUpRequest.getPassword(), userEntity.getPasswordHash())) {
+                System.out.println("Right password");
+                return tokenManager.generateToken(signUpRequest.getLogin());
+            }
+        }
+
+       throw new IllegalArgumentException("Incorrect data!");
+
+//      END
+
+//
+//        return tokenManager.generateToken(signUpRequest.getLogin());
     }
 
 
